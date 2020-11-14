17be90f0bb934fbc05ea61ffe0aaf3b18899cfd4
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-11-14 16:15:36 -0500

    Created a new function to handle milestone 2 deliverables.

---
+++ milestone_2/server/server.js
@@ -0,0 +1,78 @@
+// Paths
+const PROTO_PATH = __dirname + '/../helloworld.proto';
+const secureCertificate = __dirname + '/../cert/comp4000.com.crt';
+
+// Modules
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+const fs = require('fs');
+const {MongoClient} = require('mongodb');
+
+
+// Helper classes
+const user = require('./user');
+
+// Globals
+const URI = "mongodb://admin:admin@localhost:27017/comp4000";
+const DB_NAME = 'comp4000';
+const COLLECTION_NAME = 'clients';
+
+let clientsCollection;
+let mongoClient;
+
+function connectToDB(callback) {
+    MongoClient.connect(URI, (e, client) => {
+        const db = client.db(DB_NAME);
+        mongoClient = client;
+        clientsCollection = db.collection(COLLECTION_NAME);
+        callback();
+    });
+}
+
+function loadProto() {
+    const packageDefinition = protoLoader.loadSync(
+        PROTO_PATH,
+        {
+            keepCase: true,
+            longs: String,
+            enums: String,
+            defaults: true,
+            oneofs: true
+        });
+
+    return grpc.loadPackageDefinition(packageDefinition).helloworld;
+}
+
+function startServer(DOMAIN, PORT, hello_proto) {
+    user.saveDB(clientsCollection);
+
+    const ADDRESS = DOMAIN + PORT;
+    const server = new grpc.Server();
+
+    console.log('Server started');
+
+    const rpcMessages = {
+        signUp: user.signUp,
+        logIn: user.logIn,
+        isAuthenticated: user.isAuthenticated,
+        updatePassword: user.updatePassword,
+        deleteAccount: user.deleteAccount
+    };
+
+    server.addService(hello_proto.Greeter.service, rpcMessages);
+    server.bind(ADDRESS, grpc.ServerCredentials.createInsecure());
+    server.start();
+
+    return server;
+}
+
+function main() {
+    const PORT = ':10001';
+    const DOMAIN = 'localhost';
+    const grpc_proto = loadProto();
+    const startServerWrapper = () => startServer(DOMAIN, PORT, grpc_proto);
+    connectToDB(startServerWrapper);
+}
+
+main();
+
---
+++ milestone_2/server/user.js
@@ -0,0 +1,162 @@
+const bcrypt = require('bcrypt');
+const util = require('../common/util');
+
+module.exports = {
+    saveDB: saveDB,
+    signUp: signUp,
+    logIn: logIn,
+    isAuthenticated: isAuthenticated,
+    updatePassword: updatePassword,
+    deleteAccount: deleteAccount
+};
+
+let clientsCollection;
+
+function saveDB(clientsColl) {
+    clientsCollection = clientsColl;
+}
+
+// Sign Up User
+function signUp(call, callback) {
+    const user = {username: call.request.username, password: call.request.password};
+
+    const saveUser = () => {
+        const BCRYPT_SALT_ROUNDS = 12;
+        bcrypt.hash(user.password, BCRYPT_SALT_ROUNDS).then(hashedPwd => {
+            user.password = hashedPwd;
+            addUserToCollection(user, callback);
+        });
+    };
+
+    const rejectSaveUser = () => {
+        const signUpReply = {status: 0, message: 'Failed user already exists'};
+        callback(null, signUpReply);
+    }
+
+    clientsCollection.find({username: call.request.username}).limit(1).count().then(count => {
+        if (count < 1) {
+            console.log('Save user');
+            saveUser();
+        } else {
+            console.log('Reject user');
+            rejectSaveUser();
+        }
+    });
+}
+
+// Login User
+function logIn(call, callback) {
+    console.log('Log in');
+    const BCRYPT_SALT_ROUNDS = 12;
+    const tempUser = {username: call.request.username, password: call.request.password};
+
+    clientsCollection.findOne({username: tempUser.username}).then(user => {
+        function handleUserToken(e, token) {
+            const expirationDate = util.generateExpirationDate();
+
+            clientsCollection.updateOne(
+                {username: user.username}, {$set: {token: token, expirationDate: expirationDate}}
+            );
+
+            logInReply = {status: 1, message: token};
+            callback(null, logInReply);
+        }
+
+        if (user) {
+            bcrypt.compare(tempUser.password, user.password, function (err, isMatch) {
+                console.log(isMatch);
+                if (err) {
+                    logInReply = {status: -1, message: 'Failed something went wrong.'};
+                    callback(null, logInReply);
+                } else if (isMatch) {
+                    util.generareRandomToken(handleUserToken);
+                } else {
+                    logInReply = {status: 0, message: 'Password is not a match!'};
+                    callback(null, logInReply);
+                }
+            })
+        } else {
+            logInReply = {status: 0, message: 'User was not found.'};
+            callback(null, logInReply);
+        }
+    });
+}
+
+function updatePassword(call, callback) {
+    const username = call.request.username;
+    const token = call.request.token;
+    const newPassword = call.request.newPassword;
+
+    console.log('Updated password');
+    console.log(username);
+    console.log(token);
+    console.log(newPassword);
+
+    const BCRYPT_SALT_ROUNDS = 12;
+    const tempUser = {username: username, password: newPassword};
+
+    clientsCollection.findOne({username: tempUser.username}).then(user => {
+        let logInReply;
+
+        if (user && user.token === token) {
+
+            bcrypt.hash(newPassword, BCRYPT_SALT_ROUNDS).then(hashedPwd => {
+                // Logout user by making token invalid. Update password with the new password.
+                clientsCollection.updateOne(
+                    {username: user.username}, {$set: {token: '', password: hashedPwd}}
+                );
+                logInReply = {status: 1, message: 'Password updated.'};
+                callback(null, logInReply);
+            });
+
+
+        } else {
+            logInReply = {status: 0, message: 'Tokens don\'t match.'};
+            callback(null, logInReply);
+        }
+    });
+}
+
+
+function deleteAccount(call, callback) {
+    console.log('Delete account');
+
+    const username = call.request.username;
+    const token = call.request.token;
+    const BCRYPT_SALT_ROUNDS = 12;
+    const tempUser = {username: username};
+
+    console.log(username);
+    console.log(token);
+
+    clientsCollection.findOne({username: tempUser.username}).then(user => {
+        let logInReply;
+
+        if (user && user.token === token) {
+            clientsCollection.remove(
+                {username: user.username}
+            );
+            logInReply = {status: 1, message: 'User removed.'};
+            callback(null, logInReply);
+
+        } else {
+            logInReply = {status: 0, message: 'Tokens don\'t match.'};
+            callback(null, logInReply);
+        }
+    });
+}
+
+function addUserToCollection(user, callback) {
+    clientsCollection.insertOne(user).then(() => {
+        const response = {status: 1, message: 'Success'};
+        callback(null, response);
+    });
+}
+
+
+// This function is not being used.
+function isAuthenticated() {
+    console.log('Is authenticated');
+}
+
+
