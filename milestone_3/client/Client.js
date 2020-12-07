4b188a7696b1b8fc488aa10957237d3508a96264
Steven Stapleton <ssstevenstapleton@gmail.com>
2020-12-07 17:45:07 -0500

    Simplified client

--- milestone_3/client/Client.js
+++ milestone_3/client/Client.js
@@ -7,50 +7,46 @@ const protoLoader = require('@grpc/proto-loader');
 const fs = require('fs');
 const Fuse = require('fuse-native');
 const User = require('./User');
-
-// Helper functions
 const ops = require("./FileSystem").ops;
 
-
-// Config
-const packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {
-        keepCase: true,
-        longs: String,
-        enums: String,
-        defaults: true,
-        oneofs: true
-    });
+// Constants
+const PORT = ':10001';
+const DOMAIN = 'localhost'
+const ADDRESS = DOMAIN + PORT;
 
 // User session
 let curentUser;
 
-const hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+// The credentials part I borrowed from the following repository
+// https://github.com/gbahamondezc/node-grpc-ssl/blob/master/
+const getCredentials = () => grpc.credentials.createSsl(
+    fs.readFileSync('../certs/ca.crt'),
+    fs.readFileSync('../certs/client.key'),
+    fs.readFileSync('../certs/client.crt')
+);
 
-function main() {
-    const PORT = ':10001';
-    const DOMAIN = 'localhost'
-    const ADDRESS = DOMAIN + PORT;
+function getProto() {
+    const packageDefinition = protoLoader.loadSync(
+        PROTO_PATH,
+        {
+            keepCase: true,
+            longs: String,
+            enums: String,
+            defaults: true,
+            oneofs: true
+        });
 
-    // The credentials part I borrowed from the following repository
-    // https://github.com/gbahamondezc/node-grpc-ssl/blob/master/
-    const credentials = grpc.credentials.createSsl(
-        fs.readFileSync('../certs/ca.crt'),
-        fs.readFileSync('../certs/client.key'),
-        fs.readFileSync('../certs/client.crt')
-    );
-
-    const stub = new hello_proto.Greeter(ADDRESS, credentials);
-    start(stub);
+    return grpc.loadPackageDefinition(packageDefinition).helloworld;
 }
 
-main();
+function getStub(credentials, proto) {
+    const stub = new proto.Greeter(ADDRESS, credentials);
+    return stub;
+}
 
-function start(stub) {
-    console.log(ops);
+function moundFuse(stub) {
     ops.setClient(stub);
-    const fuse = new Fuse('./fuse', ops, { force: false, displayFolder: true });
+    const fuse = new Fuse('./fuse', ops, {force: false, displayFolder: true});
 
     fuse.mount(err => {
         if (err) throw err
@@ -59,9 +55,7 @@ function start(stub) {
         User.update(stub);
     });
 
-    process.once('SIGINT', function () {
-        unmoundFuse(fuse);
-    })
+    process.once('SIGINT', () => unmoundFuse(fuse));
 }
 
 function unmoundFuse(fuse) {
@@ -73,3 +67,14 @@ function unmoundFuse(fuse) {
         }
     })
 }
+
+function main() {
+    const credentials = getCredentials();
+    const proto = getProto();
+    const stub = getStub(credentials, proto);
+    moundFuse(stub);
+}
+
+main();
+
+
